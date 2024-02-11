48049104cbef2291d90022e4d073bf42c42506e3
Eduardo Bautista <eduardobautistauribe7@gmail.com>
2024-02-11 18:29:17 -0500

    hey

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,13 @@
-console.log('Express Tutorial')
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('./public'))
+
+app.all('*', (req,res)=>{
+    res.status(404).send('resource not found')
+})
+
+app.listen(5001,()=>{
+    console.log('serverlistening in 5001')
+})
