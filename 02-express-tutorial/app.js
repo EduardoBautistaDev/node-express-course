efe2842c62d9f25e463e91498c3588d009614aee
Eduardo Bautista <eduardobautistauribe7@gmail.com>
2024-02-11 19:19:03 -0500

    hey

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
+const {products} = require('./data.js')
 
 app.get('/',(req,res)=>{
-    res.json([{name:'john'}, {name:'susan'}])
+    res.send('<h1>home page</h1><a href="/api/products">products</a>')
 })
 
+app.get('/api/products', (req,res) => {
+    const newProducts = products.map((product)=>{
+        const {id, name, image} = product;
+        return {id, name, image}
+    })
+
+    res.json(products)
+})
+
+app.get('/api/products/:productID', (req,res) => {
+    const {productID} = req.params
+    const singleProduct = products.find((product)=>product.id === Number(productID));
+    if(!singleProduct){
+        return res.send('Product does not exist')
+    }
+    return res.json(singleProduct)
+
+})
 app.listen(5001,()=>{
     console.log('listening in port 5001')
 })
